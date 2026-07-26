a67fea1b6875f297988d40fa03e01572f9232772
Ojas-Sinha <ojassinha14@gmail.com>
2026-07-26 19:01:40 +0530

    Week-4

---
+++ Week-5/cricketapp/src/IndianPlayers.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const IndianTeam = [
+    "Sachin",
+    "Dhoni",
+    "Virat",
+    "Rohit",
+    "Yuvraj",
+    "Raina"
+];
+
+// Destructuring
+export function OddPlayers([first, , third, , fifth]) {
+    return (
+        <div>
+            <ul>
+                <li>First : {first}</li>
+                <li>Third : {third}</li>
+                <li>Fifth : {fifth}</li>
+            </ul>
+        </div>
+    );
+}
+
+export function EvenPlayers([, second, , fourth, , sixth]) {
+    return (
+        <div>
+            <ul>
+                <li>Second : {second}</li>
+                <li>Fourth : {fourth}</li>
+                <li>Sixth : {sixth}</li>
+            </ul>
+        </div>
+    );
+}
+
+// Merge Arrays
+const T20Players = [
+    "First Player",
+    "Second Player",
+    "Third Player"
+];
+
+const RanjiTrophyPlayers = [
+    "Fourth Player",
+    "Fifth Player",
+    "Sixth Player"
+];
+
+export const IndianPlayers = [
+    ...T20Players,
+    ...RanjiTrophyPlayers
+];
+
+export function ListofIndianPlayers({ IndianPlayers }) {
+    return (
+        <ul>
+            {IndianPlayers.map((player, index) => (
+                <li key={index}>
+                    Mr. {player}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export { IndianTeam };
---
+++ Week-5/cricketapp/src/ScoreBelow70.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ScoreBelow70({ players }) {
+
+    const players70 = players.filter(player => player.score <= 70);
+
+    return (
+        <ul>
+            {players70.map((player, index) => (
+                <li key={index}>
+                    Mr. {player.name} {player.score}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default ScoreBelow70;
