95b38efefc21220fd692a5846b2bebc96e901477
Ojas-Sinha <ojassinha14@gmail.com>
2026-07-26 19:17:40 +0530

    Week-5

--- Week-5/officespacerentalapp/src/App.js
+++ Week-5/officespacerentalapp/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import office from "./images/office.jpg";
 
 function App() {
+
+  const heading = "Office Space";
+
+  const officeList = [
+    {
+      Name: "DBS",
+      Rent: 50000,
+      Address: "Chennai",
+      Image: office
+    },
+    {
+      Name: "WeWork",
+      Rent: 75000,
+      Address: "Bangalore",
+      Image: office
+    },
+    {
+      Name: "Regus",
+      Rent: 45000,
+      Address: "Hyderabad",
+      Image: office
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: "40px" }}>
+
+      <h1>{heading}, at Affordable Range</h1>
+
+      {
+        officeList.map((item, index) => (
+
+          <div key={index} style={{ marginBottom: "40px" }}>
+
+            <img
+              src={item.Image}
+              alt="Office Space"
+              width="300"
+              height="200"
+            />
+
+            <h2>Name: {item.Name}</h2>
+
+            <h3
+              style={{
+                color: item.Rent <= 60000 ? "red" : "green"
+              }}
+            >
+              Rent: Rs. {item.Rent}
+            </h3>
+
+            <h3>Address: {item.Address}</h3>
+
+          </div>
+
+        ))
+      }
+
     </div>
   );
 }
